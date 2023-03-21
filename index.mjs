164fcf94c6cff13a6f9df4a905de980a838b1014
KamalEyubov <kamal.eyubov1999@gmail.com>
2023-03-21 16:56:12 +0100

    Added MongoDB driver for NodeJS

--- index.mjs
+++ index.mjs
@@ -1,5 +1,17 @@
 import * as fs from 'fs';
 
+async post(content) {
+    // TODO
+    // Find if Meta empty
+    // If Meta empty initialize it with "zero" entry
+    // Otherwise, find last entry id
+    // Update next field of the last entry in the entries
+    // Write new entry with previous field set at the last entry id
+    // Update last entry id in the meta table
+}
+
+async getEntry()
+
 export const handler = async (event) => {
     // TODO implement
     var statusCode = 404, body;
